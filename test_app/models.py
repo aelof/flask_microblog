5c62371b784ecb7302efc97ffafcf3973be9b359
alex <bestreality@mail.ru>
2021-09-23 12:11:57 +0300

    add generate and check password's hash

--- test_app/models.py
+++ test_app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from test_app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,10 +9,15 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
