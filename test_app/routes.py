e9c5447d11dddecafc9c23006f7bce8f22ced297
alex <bestreality@mail.ru>
2021-09-23 13:27:57 +0300

    add system logout

--- test_app/routes.py
+++ test_app/routes.py
@@ -1,13 +1,13 @@
 from test_app import app
 from flask import render_template, flash, redirect, url_for
 from test_app.forms import LoginForm
-from flask_login import current_user, login_user
-from test_app models import User
+from flask_login import current_user, login_user, logout_user
+from test_app.models import User
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated: # current_user is user's object. Value is db's value
+    if current_user.is_authenticated:  # current_user is user's object. Value is db's value
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -16,10 +16,15 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-            #  save current user for all future pages            
-            login_user(user, remember=form.remember_me.data) 
+            #  save current user for all future pages
+            login_user(user, remember=form.remember_me.data)
             return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/')
