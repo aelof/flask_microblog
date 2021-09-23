bf0b0b574a4176acea03a0458f8c8c1eae826605
alex <bestreality@mail.ru>
2021-09-23 13:16:46 +0300

    add user system login

--- test_app/routes.py
+++ test_app/routes.py
@@ -1,17 +1,25 @@
 from test_app import app
 from flask import render_template, flash, redirect, url_for
 from test_app.forms import LoginForm
+from flask_login import current_user, login_user
+from test_app models import User
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated: # current_user is user's object. Value is db's value
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).firts()
+        # if password wrong or user didn't exists
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+            #  save current user for all future pages            
+            login_user(user, remember=form.remember_me.data) 
+            return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-
 
 
 @app.route('/')
