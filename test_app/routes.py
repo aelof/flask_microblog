c553fe314a74a059089d126919702cf282f5e0e1
alex <bestreality@mail.ru>
2021-09-24 09:39:48 +0300

    add register form

--- test_app/routes.py
+++ test_app/routes.py
@@ -1,9 +1,10 @@
-from test_app import app
+from test_app import app, db
 from flask import render_template, flash, redirect, url_for, request 
-from test_app.forms import LoginForm
+from test_app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from test_app.models import User
 from werkzeug.urls import url_parse
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -13,7 +14,7 @@ def login():
 
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).firts()
+        user = User.query.filter_by(username=form.username.data).first()
         
         # if password wrong or user didn't exists
         if user is None or not user.check_password(form.password.data):
@@ -30,6 +31,28 @@ def login():
         return redirect(next_page)
 
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect (url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulatons, you are now a register user')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+
+
+
+
+
 
 
 @app.route('/logout')
