620becf018f27b73d968ef1b110c090612306810
alex <bestreality@mail.ru>
2021-09-28 13:39:58 +0300

    update models, add followers

--- test_app/models.py
+++ test_app/models.py
@@ -6,14 +6,26 @@ from test_app import login
 from hashlib import md5
 
 
+followers = db.Table(
+    'followers',
+     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique = True)
-    email = db.Column(db.String(120), index=True, unique = True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    folowed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -38,6 +50,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
 
 
 
