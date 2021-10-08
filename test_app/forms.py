6bc6b09944fed3cddb7e4f4964ae3f7deae952c3
alex <bestreality@mail.ru>
2021-10-08 18:12:15 +0300

    some updates

--- test_app/forms.py
+++ test_app/forms.py
@@ -33,6 +33,7 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
